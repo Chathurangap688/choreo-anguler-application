1f24e342c2d5d0b1a2847f1166e5137b0259cfad
chathurangap688 <bmcpthilakawansha@gmail.com>
2024-11-22 03:08:55 +0530

    get api res

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'choreo-anguler-application';
+  results: any;
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.getApiResponse();
+  }
+  getApiResponse() {
+    this.http.get('https://94e5cdf8-348a-4b96-8029-d31bf5e62a82.e1-us-east-azure.choreoapps.dev/hello?name=chathuranga1').subscribe((response) => {
+      this.results = response
+    })
+  }
 }
